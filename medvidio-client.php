fd146dc04976f54495a33852c93cfd51d7f5b46d
gregzuro <greg@zuro.net>
2015-04-13 23:08:52 -0700

    add gen JWT; get url from apollo;

--- medvidio-client.php
+++ medvidio-client.php
@@ -8,6 +8,65 @@ Version: 1.0
 Author URI: http://github.com/gregzuro
 */
 
+require 'vendor/autoload.php';
+
+
+function medvidio_client_get( $atts ) {
+	$a = shortcode_atts( array('id' => 0,), $atts);
+	$id = $a['id'] ;
+	if ($id == 0) {
+		return "[ MedVid.io: No Id specified ]";
+	}
+
+	global $wpdb;
+	$table_name = $wpdb->prefix . 'medvidio_videos';
+	$video = $wpdb->get_results(
+		"
+		SELECT mv_video_id, description, mv_application, mv_public_key, mv_secret_key 
+		FROM $table_name
+		WHERE id = $id
+		"
+	);
+
+//	$keys = array('1' => 'my_key', '2' => 'my_key2');
+
+	$payload = array(
+		"aud" => $video[0]->mv_public_key,
+		"iat" => time(),
+		"exp" => time()+20000,
+		"sub" => $video[0]->mv_application
+		);
+	$token = JWT::encode($payload, $video[0]->mv_secret_key, 'HS256');
+
+//	return "The public key is: " . $video[0]->mv_public_key . " !";
+
+	$service_url = "https://apollo.reeldx.com/api/v1/video/" . $video[0]->mv_video_id;
+	$curl = curl_init($service_url);
+    curl_setopt($curl, CURLOPT_TIMEOUT, 5);
+    curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+    	'Accept: application/json',
+    	'Authorization: Bearer ' . $token
+    	));
+
+
+    $curl_response = curl_exec($curl);
+	$httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+	curl_close($curl);
+
+	if ($httpcode != "200") {
+		return "[ MedVid.io: error retrieving: " . $httpcode . " ]";
+	}
+	else {
+		return "[ MedVid.io: curl result is: " . $curl_response . " ]";
+	}
+
+
+}
+add_shortcode( 'medvidio', 'medvidio_client_get');
+
 add_action('admin_menu', 'medvidio_client_admin_actions');
 function medvidio_client_admin_actions() {
 	add_options_page('MedVidioClient', 'MedVidioClient', 'manage_options', __FILE__, 'medvidio_client_admin');
@@ -23,7 +82,7 @@ function medvidio_client_admin()
 	<tr>
 	<th> WP Id </th>
 	<th> Video Id </th>
-	<th> Application </th>
+	<th> User Id </th>
 	<th> Public Key </th>
 	<th> Secret Key </th>
 	</tr>
@@ -32,7 +91,7 @@ function medvidio_client_admin()
 	<tr>
 	<th> WP Id </th>
 	<th> Video Id </th>
-	<th> Application </th>
+	<th> User Id </th>
 	<th> Public Key </th>
 	<th> Secret Key </th>
 	</tr>
@@ -41,10 +100,11 @@ function medvidio_client_admin()
 <?php
 	global $wpdb;
 
+	$table_name = $wpdb->prefix . 'medvidio_videos';
 	$medvidio_videos = $wpdb->get_results(
 		"
 		SELECT id, mv_video_id, mv_application, mv_public_key, mv_secret_key
-		from wp_medvidio_videos
+		FROM $table_name
 		"
 	);
 ?>
@@ -72,7 +132,7 @@ function medvidio_client_admin()
 register_activation_hook( __FILE__, 'medvidio_client_db_install');
 //register_activation_hook( __FILE__, 'db_install_data');
 global $medvidio_client_db_version;
-$medvidio_client_db_version = '1.0';
+$medvidio_client_db_version = '1.2';
 
 function medvidio_client_db_install() {
 	global $wpdb;
